bff5605159391117d41528207290987f48bdb661
fenglang <fenglang@littlegnal-mbp13.local>
2021-10-26 11:36:55 +0800

    WIP

--- example/lib/config/agora.config.dart
+++ example/lib/config/agora.config.dart
@@ -1,14 +1,14 @@
 /// Get your own App ID at https://dashboard.agora.io/
-const appId = YOUR_APP_ID;
+const appId = '70378ad04e1e42018ab0cd05eb2a2376';
 
 /// Please refer to https://docs.agora.io/en/Agora%20Platform/token
-const token = YOUR_TOEKN;
+const token = null;
 
 /// Your channel ID
-const channelId = YOUR_CHANNEL_ID;
+const channelId = 'testapi';
 
 /// Your int user ID
-const uid = YOUR_UID;
+const uid = 0;
 
 /// Your string user ID
-const stringUid = YOUR_STRING_UID;
+const stringUid = '0';
